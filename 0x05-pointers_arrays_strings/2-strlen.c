ae978fd7d26a9617ed365fd328afb5b841e17d02
Kalejaye <kalejayejephthah@gmail.com>
2023-09-20 17:51:57 -0700

    README.md

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,19 +1,18 @@
 /**
  * _strlen - Returns the length of a string.
- * @s: The string to measure the length of.
+ * @s: The string to be measured.
  *
  * Return: The length of the string.
  */
 int _strlen(char *s)
 {
-    int length = 0;
+	int length = 0;
 
-    while (*s != '\0')
-    {
-        length++;
-        s++;
-    }
+	while (*s)
+	{
+		length++;
+		s++;
+	}
 
-    return length;
+	return (length);
 }
-
