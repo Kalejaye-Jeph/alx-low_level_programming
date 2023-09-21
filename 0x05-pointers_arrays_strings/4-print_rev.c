4de520d5a751be1e15b580492080739820b940f5
Kalejaye <kalejayejephthah@gmail.com>
2023-09-21 07:16:19 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,11 +6,9 @@
  * @s: The string to print
  * Return: void
  */
-
 void print_rev(char *s)
-
 {
-
+	
 	int c = 0;
 
 	while (s[c] != '\0')
@@ -18,7 +16,7 @@ void print_rev(char *s)
 	c++;
 	}
 
-	for (c -= 1; c >= 0; c--)
+	for (c -= 1; c>= 0; c--)
 	{
 	_putchar(s[c]);
 	}
